d4b815f2dc0cf67f721efe3ed91aae555c0fd56b
RumaStudy <139948823+RumaStudy@users.noreply.github.com>
2023-07-26 17:25:18 +0900

    PC Complete Edition
    PC 화면 상의 구현이 완료되었습니다.
    
    -패치 예정 목록 변화 내역 안내
    기존 aside에 추가 예정이던 calender가 취소되고 기상에 대한 내용의 가시성을 증대하였습니다.
    
    패치 예정 목록
    responsive UI

--- js/clock.js
+++ js/clock.js
@@ -1,4 +1,5 @@
 const clock = doc.querySelector("#date > p");
+const datePutter = doc.querySelector("#date > span");
 
 function getTime() {
   const time = new Date();
@@ -8,8 +9,9 @@ function getTime() {
   const hour = ("0" + time.getHours()).slice(-2);
   const minutes = ("0" + time.getMinutes()).slice(-2);
   const week = time.getDay();
-  const dayOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  clock.innerHTML = `${year}. ${month}. ${date} (${dayOfWeek[week]})<br/> ${hour} : ${minutes}`;
+  const dayOfWeek = ["일", "월", "화", "수", "목", "금", "토"];
+  clock.innerHTML = `${year}. ${month}. ${date} (${dayOfWeek[week]})`;
+  datePutter.innerHTML = `${hour} : ${minutes}`;
 }
 getTime();
 setInterval(getTime, 1000);
