4b618ca310b6fd819933d141e6603954e536f398
RumaStudy <139948823+RumaStudy@users.noreply.github.com>
2023-07-26 15:46:20 +0900

    schedule Making Patch
    스케줄을 능동적으로 추가 삭제 할 수 있도록 조정했습니다.
    
    추가 예정 목록
    - 달력(캘린더)
    - 현재 일정 갯수(미완료, 완료 갯수 포함)

--- js/adder.js
+++ js/adder.js
@@ -1,10 +1,79 @@
-const addBtn = doc.querySelector(".add_schezule");
-const addPlace = doc.getElementById("add_place");
+const addBtn = doc.querySelector(".add_schezule"); // 버튼 추가 버튼
+const addPlace = doc.getElementById("add_place"); //버튼 추가할 지점
 
+/** 일정 추가 이벤트리스너 */
 addBtn.addEventListener("click", () => {
   const hasAdded = doc.createElement("div");
-  const beAdded = doc.createTextNode("추가합니다.");
-  hasAdded.append(beAdded);
-  addPlace.append(hasAdded);
+  hasAdded.classList.add("added");
+  const scheNum = addPlace.querySelectorAll(".added");
+  console.log(scheNum.length);
+  if (scheNum.length >= 5) {
+    alert("일정은 다섯개까지만! 쉬엄쉬엄해요 우리");
+  } else {
+    addPlace.append(hasAdded);
+    makingDesc(hasAdded);
+  }
 });
-addPlace.innerHTML += "<div class='adder'>추가합니다</div>";
+
+/** 일정 추가 내부 제작 */
+function makingDesc(whereDiv) {
+  /* 체크, 삭제 버튼 */
+  const removerBtn = doc.createElement("button");
+  const checkerBtn = doc.createElement("button");
+  removerBtn.classList.add("remover");
+  checkerBtn.classList.add("checker");
+
+  /* Input & Label */
+  const inputPlacer = doc.createElement("div");
+  inputPlacer.classList.add("sche_place");
+  const titlePutter = doc.createElement("input");
+  titlePutter.placeholder = "제목을 입력해주세요.";
+  const titleConfirmer = doc.createElement("label");
+  const memoPutter = doc.createElement("input");
+  memoPutter.placeholder = "메모를 입력해주세요.";
+  const memoConfirmer = doc.createElement("label");
+
+  /** 삭제버튼 함수 */
+  removerBtn.addEventListener("click", () => {
+    whereDiv.remove();
+  });
+  /** 체크버튼 함수 */
+  checkerBtn.addEventListener("click", (e) => {
+    let confirmChecker = e.target.classList.contains("checked");
+    if (!confirmChecker) {
+      e.target.classList.add("checked");
+      titleConfirmer.style.textDecoration = "line-through";
+      memoConfirmer.style.textDecoration = "line-through";
+    } else {
+      e.target.classList.remove("checked");
+      titleConfirmer.style.textDecoration = "none";
+      memoConfirmer.style.textDecoration = "none";
+    }
+  });
+  function putterAction(things) {
+    things.addEventListener("change", (e) => {
+      const textInput = e.target.value;
+      const nextLabel = e.target.nextElementSibling;
+      if (textInput.length === 0) {
+      } else {
+        nextLabel.innerText = textInput;
+        console.log(nextLabel);
+        e.target.style.display = "none";
+      }
+    });
+  }
+  putterAction(memoPutter);
+  putterAction(titlePutter);
+  /* input 부분 제작 */
+  inputPlacer.append(titlePutter);
+  inputPlacer.append(titleConfirmer);
+  inputPlacer.append(memoPutter);
+  inputPlacer.append(memoConfirmer);
+  whereDiv.append(removerBtn);
+  whereDiv.append(checkerBtn);
+  whereDiv.append(inputPlacer);
+}
+
+/** 삭제 버튼 */
+
+/** 체크 버튼 */
